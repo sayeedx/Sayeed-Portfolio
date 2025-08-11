4f06d551f2ad9862b877ba8eb11f638ddb5325ab
sayeed <asayeed216@gmail.com>
2025-08-11 00:38:38 -0400

    inital stage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React/*, { useState, useEffect }*/ from 'react'
+import { Link } from 'react-router-dom'
+import { styles } from '../styles'
+/*import { navLinks } from '../constants'*/
+import { logo/*, menu, close*/ } from '../assets'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link to="/"
+        className="flex items-center gap-2"
+        onClick={() => {
+          SecurityPolicyViolationEvent("");
+          window.scrollTo(0, 0);
+        }}>
+          <img src={logo} alt="logo" className="w-6 h-6 object-contain" />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>
+            Sayeed <span className='sm:block hidden'>
+              | JavaScript Mastery</span></p>
+        </Link>
+        <p className="text-red-500">asdsa</p>
+      </div>
+    </nav>
   )
 }
 
